bcca17bb5bb5dfec6a3ac222bee2056dd466bdcc
shadow_walker <shadowwalkersb@gmail.com>
2018-03-11 19:43:45 -0500

    toolchain file for linux

--- recipes/eman/build.sh
+++ recipes/eman/build.sh
@@ -8,7 +8,11 @@ rm -rf $build_dir
 mkdir -p $build_dir
 cd $build_dir
 
-cmake $SRC_DIR
+if [ "$(uname -s)" == "Linux" ];then
+    cmake $SRC_DIR -DCMAKE_TOOLCHAIN_FILE=${RECIPE_DIR}/cross-linux.cmake
+else
+    cmake $SRC_DIR
+fi
 
 make -j${CPU_COUNT}
 make install
