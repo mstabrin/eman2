ba088947d1849f8e49241770700ad8d1a5e130f2
shadow_walker <shadowwalkersb@gmail.com>
2018-05-03 00:35:04 -0500

    move around

--- recipes/eman/build.sh
+++ recipes/eman/build.sh
@@ -2,15 +2,15 @@
 
 set -xe
 
-if [ "$(uname -s)" == "Darwin" ];then
-    LDFLAGS=${LDFLAGS/-Wl,-dead_strip_dylibs/}
-fi
-
 build_dir="${SRC_DIR}/../build_eman"
 
 rm -rf $build_dir
 mkdir -p $build_dir
 cd $build_dir
+
+if [ "$(uname -s)" == "Darwin" ];then
+    LDFLAGS=${LDFLAGS/-Wl,-dead_strip_dylibs/}
+fi
 
 if [ "$(uname -s)" == "Linux" ];then
     cmake $SRC_DIR -DCMAKE_TOOLCHAIN_FILE=${RECIPE_DIR}/cross-linux.cmake
