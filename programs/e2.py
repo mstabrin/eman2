a567d757dde825f7502fcdce45b0143f193b727b
EMAN Build <eman2@localhost.localdomain>
2018-05-04 13:49:15 -0500

    stash

--- programs/e2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from __future__ import print_function
-
-import os
-
-e2real = os.path.join(os.path.abspath(os.path.dirname(__file__)), "e2_real.py")
-os.execlp("ipython","ipython","-i","--gui=qt",e2real)
--- tests/run_tests.bat
+++
@@ -1,4 +0,0 @@
-e2version.py
-e2speedtest.py
-
-python tests\test_EMAN2DIR.py
