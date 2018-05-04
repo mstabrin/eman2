3368faa85ac1c8642a590f34904f917096acfd3a
EMAN Build <eman2@localhost.localdomain>
2018-05-04 15:17:39 -0500

    stash

--- cmake/FindNosetests.cmake
+++
@@ -1,8 +0,0 @@
-find_program(NOSETESTS_EXECUTABLE nosetests
-		PATHS ${CONDA_PREFIX}/bin ${CONDA_PREFIX}/Scripts ENV PATH
-		)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Nosetests
-		REQUIRED_VARS NOSETESTS_EXECUTABLE
-		)
