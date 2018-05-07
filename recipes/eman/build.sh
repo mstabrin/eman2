fd8e13a85448aded140ba45cd08d30ef587bc0a5
shadow_walker <shadowwalkersb@gmail.com>
2018-05-06 21:14:19 -0500

    remove flag from LDFLAGS: -Wl,-dead_strip_dylibs

--- recipes/eman/build.sh
+++ recipes/eman/build.sh
@@ -8,6 +8,10 @@ rm -rf $build_dir
 mkdir -p $build_dir
 cd $build_dir
 
+if [ "$(uname -s)" == "Darwin" ];then
+    LDFLAGS=${LDFLAGS/-Wl,-dead_strip_dylibs/}
+fi
+
 cmake $SRC_DIR
 
 make -j${CPU_COUNT}
