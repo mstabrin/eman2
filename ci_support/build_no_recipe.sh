00d615b6141e1ed8fb49b12e417a1fca50aff512
shadow_walker <shadowwalkersb@gmail.com>
2018-05-07 22:53:42 -0500

    fix too many levels of symbolic links

--- ci_support/build_no_recipe.sh
+++ ci_support/build_no_recipe.sh
@@ -29,6 +29,10 @@ rm -rf $build_dir
 mkdir -p $build_dir
 cd $build_dir
 
+if [ "$(uname -s)" == "Linux" ];then
+    rm -vf ${CONDA_PREFIX}/include/GL
+fi
+
 cmake ${src_dir}
 make -j${CPU_COUNT}
 make install
