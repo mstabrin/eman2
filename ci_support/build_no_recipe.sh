11ba010c62fd6d0c8048890f286a79c4a2fe4b5d
shadow_walker <shadowwalkersb@gmail.com>
2018-05-09 12:44:32 -0500

    CI support [ci skip]

--- ci_support/build_no_recipe.sh
+++ ci_support/build_no_recipe.sh
@@ -1,23 +1,31 @@
 #!/usr/bin/env bash
 
+set -xe
+
 if [ ! -z ${CI} ];then
     source ci_support/setup_conda.sh
 
     # Following Wiki instructions at
     # http://blake.bcm.edu/emanwiki/EMAN2/COMPILE_EMAN2_ANACONDA
-    if [ "$(uname -s)" != "Darwin" ];then
-        conda install --yes --quiet eman-deps="*"="np18*" -c cryoem -c defaults -c conda-forge
-    else
-        conda install --yes --quiet eman-deps -c cryoem -c defaults -c conda-forge
-    fi
+    conda install eman-deps=9 -c cryoem -c defaults -c conda-forge --yes --quiet
 fi
 
 # Build and install eman2
-export CPU_COUNT=2
+rm -vf ${CONDA_PREFIX}/bin/e2*.py
 
-export SRC_DIR=${PWD}
-export PREFIX=${PWD}
-bash ${SRC_DIR}/recipes/eman/build.sh
+conda info -a
+conda list
+
+build_dir="../build_eman"
+src_dir=${PWD}
+
+rm -rf $build_dir
+mkdir -p $build_dir
+cd $build_dir
+
+cmake ${src_dir}
+make -j4
+make install
 
 # Run tests
-bash ${SRC_DIR}/tests/run_tests.sh
+bash ${src_dir}/tests/run_tests.sh
--- ci_support/build_recipe.sh
+++ ci_support/build_recipe.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
+set -x
+
 if [ ! -z ${CI} ];then
     source ci_support/setup_conda.sh
 
-    conda install conda-build=2 -c defaults --yes --quiet
+    conda install conda-build=3 -c defaults --yes --quiet
 fi
 
 export CPU_COUNT=2
-export NUMPY_VERSION=1.9
 
-if [ "$(uname -s)" != "Darwin" ];then
-    conda build recipes/eman -c cryoem -c defaults -c conda-forge
-else
+if [ "$(uname -s)" == "Darwin" ];then
     export EMAN_TEST_SKIP=1
-    conda build recipes/eman -c cryoem -c defaults -c conda-forge
 fi
+
+conda info -a
+conda list
+conda render recipes/eman
+
+conda build recipes/eman -c cryoem -c defaults -c conda-forge --quiet
